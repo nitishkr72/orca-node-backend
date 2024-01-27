b5f7292608efdad0f77533f047377afba256abed
Nitish Kumar <nitish_tu@outlook.com>
2024-01-27 16:25:34 +0530

    minor bug removed

--- src/controllers/issueController.ts
+++ src/controllers/issueController.ts
@@ -5,11 +5,11 @@ export const get = async (_: any, res: any) => {
   res.json(JSON.stringify(issues));
 };
 
-export const post = (req: any, res: any) {
-    const issue_json = req.body;
+export const post = (req: any, res: any) => {
+  const issue_json = req.body;
 
-    const issue = new Issue(issue_json);
-    issue.save()
+  const issue = new Issue(issue_json);
+  issue.save();
 
-    res.send("OK")
-}
+  res.send("OK");
+};
